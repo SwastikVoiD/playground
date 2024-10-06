b363bb664bcd20f054e2b73e22669a6bcb4763f9
SwastikVoiD <swastikdebesh@gmail.com>
2024-10-06 11:55:54 +0530

    Solved 1_5.py

--- Python/1_5.py
+++ Python/1_5.py
@@ -9,3 +9,17 @@ ad
 bc 
 bd
 """
+dick = {'1':['a','b'], '2':['c','d']}
+dick = list(dick.values())
+cross_set = set()
+for i in dick:
+    dick.remove(i)
+    for j in dick:
+        if i != j:
+            for k in range(len(i)):
+                for l in range(len(j)):
+                    cross_set.add(i[k]+j[l])
+
+
+for i in cross_set:
+    print(i)
